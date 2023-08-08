70202dc1d74633463806875558682839a386cb3e
hainguyenduy <hainguyenduy362@gmail.com>
2023-08-09 01:33:14 +0700

    first commit

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,7 @@ import { Inter } from 'next/font/google'
 import { ClerkProvider } from '@clerk/nextjs'
 
 import { ModalProvider } from '@/providers/modal-provider'
+import { ToasterProvider } from '@/providers/toast-provider'
 
 import './globals.css'
 
@@ -22,6 +23,8 @@ export default function RootLayout({
     <ClerkProvider>
       <html lang="en">
         <body className={inter.className}>
+          <ToasterProvider />
+          <ModalProvider />
           {children}
         </body>
       </html>
--- components/modals/store-modal.tsx
+++ components/modals/store-modal.tsx
@@ -1,11 +1,17 @@
 "use client";
 
-import * as z from "zod"
+import * as z from "zod";
 import { useForm } from "react-hook-form";
+import { zodResolver } from "@hookform/resolvers/zod";
 
-import { zodResolver } from "@hookform/resolvers/zod"
-import { useStoreModal } from "@/hooks/use-store-modal"
+import { useStoreModal } from "@/hooks/use-store-modal";
 import { Modal } from "@/components/ui/modal";
+import { Form, FormControl, FormField, FormItem, FormLabel, FormMessage } from "../ui/form";
+import { Button } from "../ui/button";
+import { Input } from "../ui/input";
+import { useState } from "react";
+import axios from "axios";
+import { toast } from "react-hot-toast";
 
 const formSchema = z.object({
   name: z.string().min(1),
@@ -14,6 +20,7 @@ const formSchema = z.object({
 export const StoreModal = () => {
   const storeModal = useStoreModal();
 
+  const [loading, setLoading] = useState(false);
   const form = useForm<z.infer<typeof formSchema>>({
     resolver: zodResolver(formSchema),
     defaultValues: {
@@ -21,15 +28,60 @@ export const StoreModal = () => {
     },
   });
 
-  
+  const onSubmit = async (values: z.infer<typeof formSchema>) => {
+    try {
+      setLoading(true);
+
+      const response = await axios.post('/api/stores', values);
+      toast.success("Cửa hàng đã được tạo");
+      
+    } catch (error) {
+      toast.error("Đã xảy ra sự cố.");
+      (error);
+    } finally {
+      setLoading(false);
+    }
+  }
+
   return (
-  <Modal 
+    <Modal
     title="Create store"
     description="Add a new store to manage products and categories"
     isOpen={storeModal.isOpen}
     onClose={storeModal.onClose}
-  >
-    Future Create Store Form
-  </Modal>
+    >
+      <div className="space-y-4 py-2 pb-4">
+        <Form {...form}>
+          <form onSubmit={form.handleSubmit(onSubmit)}>
+            <FormField 
+            control={form.control}
+            name="name"
+            render={({ field }) => (
+              <FormItem>
+                <FormLabel>Name</FormLabel>
+                <FormControl>
+                  <Input
+                   disabled={loading}
+                   placeholder="E-Commerce" 
+                   {...field} />
+                </FormControl>
+                <FormMessage />
+              </FormItem>
+            )}
+            />
+            <div className="pt-6 space-x-2 flex items-center justify-end w-full">
+              <Button
+              disabled={loading}
+              variant="outline"
+              onClick={storeModal.onClose}
+              >
+                Cancel
+              </Button>
+              <Button disabled={loading} type="submit">Continue</Button>
+            </div>
+          </form>
+        </Form>
+      </div>
+    </Modal>
   );
 };
